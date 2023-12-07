f4ae9c3763d4ebbcc202aaeecd8311a3f0244519
Miguel <mriosecooss@gmail.com>
2023-12-06 23:11:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+module.exports = { fromEuroToDollar };git 
+
+// console.log("Hello World")
+
+// const sum = (a,b) => {
+//     return a + b
+// }
+
+// console.log(sum(5425,45877))
+// module.exports = { sum };
